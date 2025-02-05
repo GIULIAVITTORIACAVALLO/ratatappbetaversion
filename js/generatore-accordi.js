4ba67cc9ed9ddddd82e58984c8c7fc66f8c5e461
GIULIAVITTORIACAVALLO <giuliavittoria.cavallo@hotmail.com>
2025-02-05 12:20:57 +0100

    Update generatore-accordi.js

--- js/generatore-accordi.js
+++ js/generatore-accordi.js
@@ -1,35 +1,22 @@
-document.getElementById("generaAccordi").addEventListener("click", function () {
+function generaAccordi() {
     const tonalita = document.getElementById("tonalita").value;
-    const tipoScala = document.getElementById("tipoScala").value;
-
-    const scale = {
-        maggiore: {
-            C: ["C", "D", "E", "F", "G", "A", "B"],
-            D: ["D", "E", "F#", "G", "A", "B", "C#"],
-            E: ["E", "F#", "G#", "A", "B", "C#", "D#"],
-            F: ["F", "G", "A", "A#", "C", "D", "E"],
-            G: ["G", "A", "B", "C", "D", "E", "F#"],
-            A: ["A", "B", "C#", "D", "E", "F#", "G#"],
-            B: ["B", "C#", "D#", "E", "F#", "G#", "A#"],
-        },
-        minore: {
-            C: ["Cm", "Ddim", "D#", "Fm", "Gm", "G#", "A#"],
-            D: ["Dm", "Edim", "F", "Gm", "Am", "A#", "C"],
-            E: ["Em", "F#dim", "G", "Am", "Bm", "C", "D"],
-            F: ["Fm", "Gdim", "G#", "A#m", "Cm", "C#", "D#"],
-            G: ["Gm", "Adim", "A#", "Cm", "Dm", "D#", "F"],
-            A: ["Am", "Bdim", "C", "Dm", "Em", "F", "G"],
-            B: ["Bm", "C#dim", "D", "Em", "F#m", "G", "A"],
-        },
+    const progressioni = {
+        "C": [["C", "G", "Am", "F"], ["C", "Em", "F", "G"], ["C", "Am", "Dm", "G"]],
+        "Cm": [["Cm", "Ab", "Bb", "Eb"], ["Cm", "Gm", "Ab", "Bb"], ["Cm", "Fm", "G", "Cm"]],
+        "D": [["D", "A", "Bm", "G"], ["D", "F#m", "G", "A"], ["D", "Bm", "Em", "A"]],
+        "Dm": [["Dm", "Bb", "C", "F"], ["Dm", "Am", "Bb", "C"], ["Dm", "Gm", "A", "Dm"]],
+        "E": [["E", "B", "C#m", "A"], ["E", "G#m", "A", "B"], ["E", "C#m", "F#m", "B"]],
+        "Em": [["Em", "C", "D", "G"], ["Em", "Bm", "C", "D"], ["Em", "Am", "B", "Em"]],
+        "F": [["F", "C", "Dm", "Bb"], ["F", "Am", "Bb", "C"], ["F", "Dm", "Gm", "C"]],
+        "Fm": [["Fm", "Db", "Eb", "Ab"], ["Fm", "Cm", "Db", "Eb"], ["Fm", "Bbm", "C", "Fm"]],
+        "G": [["G", "D", "Em", "C"], ["G", "Bm", "C", "D"], ["G", "Em", "Am", "D"]],
+        "Gm": [["Gm", "Eb", "F", "Bb"], ["Gm", "Dm", "Eb", "F"], ["Gm", "Cm", "D", "Gm"]],
+        "A": [["A", "E", "F#m", "D"], ["A", "C#m", "D", "E"], ["A", "F#m", "Bm", "E"]],
+        "Am": [["Am", "F", "G", "C"], ["Am", "Em", "F", "G"], ["Am", "Dm", "E", "Am"]],
+        "B": [["B", "F#", "G#m", "E"], ["B", "D#m", "E", "F#"], ["B", "G#m", "C#m", "F#"]],
+        "Bm": [["Bm", "G", "A", "D"], ["Bm", "F#m", "G", "A"], ["Bm", "Em", "F#", "Bm"]]
     };
 
-    const progressione = [];
-    const scalaSelezionata = scale[tipoScala][tonalita];
-    
-    for (let i = 0; i < 4; i++) {
-        let accordoCasuale = scalaSelezionata[Math.floor(Math.random() * scalaSelezionata.length)];
-        progressione.push(accordoCasuale);
-    }
-
-    document.getElementById("risultatoAccordi").innerText = progressione.join(" - ");
-});
+    const progressioneCasuale = progressioni[tonalita][Math.floor(Math.random() * progressioni[tonalita].length)];
+    document.getElementById("outputAccordi").innerText = "Progressione: " + progressioneCasuale.join(" - ");
+}
